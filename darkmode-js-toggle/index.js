37d161bde151ab6e3b9b14b0fc37642b1bcb453d
Martin Fencl <martin@layeredcode.com>
2022-05-18 15:11:46 -0500

    Tidy up, remove redundant comments
    Let the code itself tell the story

--- darkmode-js-toggle/index.js
+++ darkmode-js-toggle/index.js
@@ -1,37 +1,21 @@
-// check for saved 'darkModeOn' in localStorage
+const btnDarkModeToggle = document.getElementById('btn-dark-mode-toggle')
 let darkModeOn = localStorage.getItem('darkModeOn')
 
-const btnDarkModeToggle = document.getElementById('btn-dark-mode-toggle')
+if (darkModeOn) { turnDarkModeOn() }
 
 function turnDarkModeOn() {
-  // 1. Add the class to the body
   document.body.classList.add('dark-mode')
-  // 2. Update darkMode in localStorage
   localStorage.setItem('darkModeOn', 'on')
 }
 
 function turnDarkModeOff() {
-  // 1. Remove the class from the body
   document.body.classList.remove('dark-mode')
-  // 2. Update darkModeOn in localStorage
   localStorage.removeItem('darkModeOn')
 }
 
-// If the user already visited and enabled darkModeOn
-// start things off with it on
-if (darkModeOn) {
-  turnDarkModeOn()
-}
-
-// When someone clicks the button
 btnDarkModeToggle.addEventListener('click', () => {
-  // get their darkModeOn setting
   darkModeOn = localStorage.getItem('darkModeOn')
 
-  // and toggle it
-  if (darkModeOn) {
-    turnDarkModeOff()
-  } else {
-    turnDarkModeOn()
-  }
+  if (darkModeOn) { turnDarkModeOff() }
+  else { turnDarkModeOn() }
 })
