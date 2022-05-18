fadce53a8c0f8e8318aecbf86349d0a7f8055225
Martin Fencl <martin@layeredcode.com>
2022-05-18 14:54:31 -0500

    Rename JS functions & variables
    Better code readability

--- darkmode-js-toggle/index.js
+++ darkmode-js-toggle/index.js
@@ -1,37 +1,37 @@
-// check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem('darkMode')
+// check for saved 'darkModeOn' in localStorage
+let darkModeOn = localStorage.getItem('darkModeOn')
 
 const btnDarkModeToggle = document.querySelector('#btn-dark-mode-toggle')
 
-const enableDarkMode = () => {
+const turnDarkModeOn = () => {
   // 1. Add the class to the body
   document.body.classList.add('dark-mode')
   // 2. Update darkMode in localStorage
-  localStorage.setItem('darkMode', 'on')
+  localStorage.setItem('darkModeOn', 'on')
 }
 
-const disableDarkMode = () => {
+const turnDarkModeOff = () => {
   // 1. Remove the class from the body
   document.body.classList.remove('dark-mode')
-  // 2. Update darkMode in localStorage
-  localStorage.removeItem('darkMode')
+  // 2. Update darkModeOn in localStorage
+  localStorage.removeItem('darkModeOn')
 }
 
-// If the user already visited and enabled darkMode
+// If the user already visited and enabled darkModeOn
 // start things off with it on
-if (darkMode) {
-  enableDarkMode()
+if (darkModeOn) {
+  turnDarkModeOn()
 }
 
 // When someone clicks the button
 btnDarkModeToggle.addEventListener('click', () => {
-  // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode')
+  // get their darkModeOn setting
+  darkModeOn = localStorage.getItem('darkModeOn')
 
   // and toggle it
-  if (darkMode) {
-    disableDarkMode()
+  if (darkModeOn) {
+    turnDarkModeOff()
   } else {
-    enableDarkMode()
+    turnDarkModeOn()
   }
 })
