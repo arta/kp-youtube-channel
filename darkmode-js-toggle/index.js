1efa04f0d021fcde0d782fe00b0e6bc554eeceb7
Martin Fencl <martin@layeredcode.com>
2022-05-18 20:03:44 -0500

    Rewrite dark mode toggle as a true toggle
    Way shorter, but not as easy to read. Hm ..

--- darkmode-js-toggle/index.js
+++ darkmode-js-toggle/index.js
@@ -1,24 +1,51 @@
 const btnDarkModeToggle = document.getElementById('btn-dark-mode-toggle')
+const bodyClassList = document.body.classList
 
-let darkModeOn = localStorage.getItem('darkMode')
-
-function turnDarkModeOn() {
-  document.body.classList.add('dark-mode')
-  localStorage.setItem('darkMode', 'on')
-}
-
-function turnDarkModeOff() {
-  document.body.classList.remove('dark-mode')
-  localStorage.removeItem('darkMode')
-}
+let darkModeIsPersisted = localStorage.getItem('darkMode')
 
 function toggleDarkMode() {
-  darkModeOn = localStorage.getItem('darkMode')
+  // Confusing to go thru in mind (w/o debugger) on index.html (re)load, so:
+  // : when index (re)loads, bodyClassList never has `dark-mode`
+  // : the .toggle() here adds `dark-mode` on every index (re)load
+  bodyClassList.toggle('dark-mode')
 
-  if (darkModeOn) { turnDarkModeOff() }
-  else { turnDarkModeOn() }
+  if (bodyClassList.contains('dark-mode')) {
+    if (!darkModeIsPersisted) localStorage.setItem('darkMode', 'on')
+  }
+  else localStorage.removeItem('darkMode')
 }
 
-if (darkModeOn) { turnDarkModeOn() }
+if (darkModeIsPersisted) toggleDarkMode()
 
 btnDarkModeToggle.addEventListener('click', toggleDarkMode)
+
+
+// =============================================================================
+// Wordier, but easier to read & understand, so probably better:
+// =============================================================================
+
+// const btnDarkModeToggle = document.getElementById('btn-dark-mode-toggle')
+// const bodyClassList = document.body.classList
+
+// let darkModeOn = localStorage.getItem('darkMode')
+
+// function turnDarkModeOn() {
+//   bodyClassList.add('dark-mode')
+//   localStorage.setItem('darkMode', 'on')
+// }
+
+// function turnDarkModeOff() {
+//   bodyClassList.remove('dark-mode')
+//   localStorage.removeItem('darkMode')
+// }
+
+// function toggleDarkMode() {
+//   darkModeOn = localStorage.getItem('darkMode')
+
+//   if (darkModeOn) { turnDarkModeOff() }
+//   else { turnDarkModeOn() }
+// }
+
+// if (darkModeOn) { turnDarkModeOn() }
+
+// btnDarkModeToggle.addEventListener('click', toggleDarkMode)
